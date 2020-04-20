3dde5047d088738b47ac6b043fdeb4276a54d364
java1008 <hk109@126.com>
2020-04-20 10:58:23 +0800

    小王增加for循环

--- src/main/java/com/offcn/pay/PayServiceImpl.java
+++ src/main/java/com/offcn/pay/PayServiceImpl.java
@@ -1,8 +1,16 @@
 package com.offcn.pay;
 
 public class PayServiceImpl implements PayService {
+    /**
+     * 小王修改代码，增加for循环
+     * @param money
+     */
     @Override
     public void pay(Double money) {
-        System.out.println("支付成功:"+money);
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("支付成功:"+money+i);
+        }
+
+
     }
 }
