ea01607e1a12d40508403ea7d5f208d6d2f1e11a
java1008 <hk109@126.com>
2020-04-20 11:00:34 +0800

    修理修改代码

--- src/main/java/com/offcn/pay/PayServiceImpl.java
+++ src/main/java/com/offcn/pay/PayServiceImpl.java
@@ -3,6 +3,8 @@ package com.offcn.pay;
 public class PayServiceImpl implements PayService {
     @Override
     public void pay(Double money) {
-        System.out.println("支付成功:"+money);
+        //小李修改代码
+        System.out.println("支付成功:"+money+"小李修改");
+
     }
 }
